6e4710ca582d312c34f118bb09713c1e73e8926e
iceberg-13 <zaur.tsitskiev@ya.ru>
2023-07-29 17:56:38 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+a = '10';
+alert(a);
+
+a = '20';
+alert(a);
+
+
+iphoneRelease = '2007';
+alert(iphoneRelease);
+
+
+jsCreator = 'Brendan Eich';
+alert(jsCreator);
+
+
+ten = 10;
+two = 2;
+alert(ten + two);
+alert(ten - two);
+alert(ten * two);
+alert(ten / two);
+
+
+result = 2 ** 5;
+alert(result);
+
+
+a = 9;
+b = 2;
+result = a % b;
+alert(result);
+
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+++num;
+-num;
+alert(num);
+
+
+age = prompt("Сколько вам лет?");
+alert(age);
+
+
+const user = {
+    name: 'Alex',
+    age: 33,
+    isAdmin: true
+}
+
+
+user['city of residence'] = true;
+
+
+user['age'] = 35;
+
+
+delete user['city of residence'];
+
+
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+console.log(user[info]);
+alert(user[info]);
+
+
+let userName = prompt('Введите ваше имя');
+alert(`Привет, ${userName}!`);
+
+
+
+
+
+
+
+
+
+
+
+
